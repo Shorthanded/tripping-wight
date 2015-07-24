a49e5246758a69f486a74dd2c0db4c50681d4b76
Edwin Galloway <edwin.galloway@gmail.com>
2015-07-24 15:46:10 -0500

    update
    update fast_exponentiation

--- fast_exponentiation.c
+++ fast_exponentiation.c
@@ -1,6 +1,6 @@
 /**********************************************************************
  * To try to improve this program, we look for a more general invariant
- * that will still imply the foal y=x^n when k=0.
+ * that will still imply the goal y=x^n when k=0.
  *
  * One possibility is is to introduce an additional variable z, and use
  * y*z^k=x^n instead of y*x^k=x^n.
@@ -19,6 +19,8 @@
  * 
  * Thus decreasing k by one and multiplying y*z maintains the invariant 
  * 
+ * k=k-1; y = y*z (S_)
+ *
  * But this replacement takes no advantage of the more general           
  * invariant. We can take advantage of 
  * x^(2*m) = (x*x)^m when m>=0
@@ -31,7 +33,7 @@
  * 
  * This suggests a version of "Decrease k while maintaining invariant"
  * 
- * k=k/2; z=z*z;
+ * k=k/2; z=z*z; (Sdiv)
  *
  * But this only works when k is even. The first solution you might
  * consider is 
@@ -41,24 +43,48 @@
  * 	} else {
  *		k=k/2; z=z*z;
  *	}
+ *	(Sbranch)
  *
  * A more subtle approach devised by E Djikstra is instead of branching,
  * to make k even:
  *
  * 	"make k even while maintaining invariant";
- *	// y*
+ *	{y*z^k=x^n && k>=0 && even(k)}
+ *	k=k/2; z=z*z;
+ *
+ * S_ provides a way to satisfy "make k even while maintaining invariant"
+ * If k is odd, do S_ else do nothing, So:
+ *
+ * 	if ( (odd(k) ) { k=k-1; y=y*z; }
+ *	{y*z^k=x^n && k>=0 && even(k)}
+ *	k=k/2; z=z*z;
+ *	(Smakeeven) 		
+ *
+ * Considering the running time of each of these options you will have:
+ * 
+ * a + b * n 
+ *
+ * a : the maximum time time for the linear initialization
+ * 	 portion of the program
+ * b : the maximum time for the execution of the loop
+ * n : the number of times the loop executes
+ *
+ * 
  *
  *********************************************************************/
 #include <stdio.h>
 
 int main (int argc, char *argv[])
 {
+  
   int x, y, k, n, z;
   // x >= 0
   k=n; y=1; z=x;
   // whileinv: y*z^k=x^n && k>=0
   while(k!=0){
-	k-=1; y=y*z;   
+  	if ( (odd(k) ) { k=k-1; y=y*z; }
+        //y*z^k=x^n && k>=0 && even(k)
+ 	k=k/2; z=z*z;
 	}
   // y=x^n
   return 0;
